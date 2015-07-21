65147daadb6f76dfbe581174ca04ba0d4f1fa9b5
Marc Costello <cossie7722@gmail.com>
2015-07-21 19:25:15 +0100

    Nailed user input

--- lib/player.ex
+++ lib/player.ex
@@ -1,9 +1,17 @@
 defmodule Player do
    defstruct [:type, :colour]
 
-   def receive_input(prompt // "Select a column 1-8") do
+   def receive_input(prompt \\ "Select a column 1-8: ") do
        input = IO.gets(prompt) |> String.replace "\n", ""
-       {inputInt, _rem} = Integer.parse input
-       inputInt
+       case Integer.parse input do
+         :error ->
+           print_not_valid_input(input)
+           receive_input()
+         {inputInt, _rem} -> inputInt
+       end
+   end
+
+   defp print_not_valid_input(input) do
+     IO.puts "#{input} is not a valid input"
    end
 end
