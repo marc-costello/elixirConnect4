d1f7991ece9f63056a1bc5322118544da351879e
Marc Costello <cossie7722@gmail.com>
2015-07-29 17:29:07 +0100

    diagonal win. still trying to get the recursion right, it's much more primitive than f# or haskells

--- lib/detector.ex
+++ lib/detector.ex
@@ -1,16 +1,53 @@
 defmodule Detector do
+   alias GameSettings, as: GS
 
    @type game_state_result :: {Atom.t, Atom.t, Atom.t}
 
-   @spec game_state(List.t, Atom.t, {Integer, Integer}) :: game_state_result
-   def game_state(board, colour, coord) do
+   @spec game_state(List.t, {Integer, Integer}, Atom.t) :: game_state_result
+   def game_state(board, coord, colour) do
      board
-     |> vertical_win? player, colour
-     |> horizontal_win? player, colour
-     |> diagonal_win? player, colour, coord
+     |> vertical_win? colour
+     |> horizontal_win? colour
+     |> diagonal_win? colour, coord
    end
 
-   defp is_row_a_winner?(row, colour) do
+   def vertical_win?(board, colour) do
+      Enum.any? board, fn (row) ->
+         is_group_a_winner? row, colour
+      end
+   end
+
+   def horizontal_win?(board, colour) do
+      horizontal_board = Board.convert(board, :horizontal)
+      Enum.any? board, fn (row) ->
+         is_group_a_winner? row, colour
+      end
+   end
+
+   def diagonal_win?(board, colour, coord) do
+      {move_column_index, move_row_index} = coord
+      flat_board = List.flatten board
+      max_grid_index = (length flat_board) - 1
+      starting_index = (move_column_index * GS.no_columns) + move_row_index
+
+      indexes = fn (increment, starting_pos) ->
+         
+         loop = fn (acc_index) ->
+            case acc_index do
+               i when i < 0 or i > max_grid_index -> []
+               i -> i ++ loop.(i + increment)
+            end
+         end
+         loop.(starting_pos)
+      end
+
+      [6,8,-6,-8]
+      |> Enum.map indexes.(starting_index)
+      |> Enum.map fn (l) -> convert_indexes_into_grid_entries(l, flat_board) end
+      |> Enum.any? fn (row) -> is_group_a_winner(row, colour) end
+   end
+
+   defp is_group_a_winner?(row, colour) do
      winning_count = row |> List.foldl 0, fn (x, acc) ->
        if x == colour, do: acc + 1, else: 0
      end
@@ -18,6 +55,16 @@ defmodule Detector do
       x when x >= 4 -> true
       _ -> false
      end
+   end
+
+   defp convert_indexes_into_grid_entries(indexes, flat_board) do
+       Enum.map indexes, fn (i) ->
+          elem flat_board, i
+       end
+   end
+
+   defp diagonal_indexes(acc_index) do
+
    end
 end
 
